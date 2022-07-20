020a1534652b8205ca002e6badfa908723745f42
Zaidoun Moez <81720068+zaidoune1@users.noreply.github.com>
2022-07-20 22:16:26 +0200

    Update post.routes.js

--- Groupomania_back/routes/post.routes.js
+++ Groupomania_back/routes/post.routes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer();
-//const auth = require("../middleware/auth.middleware");
+const auth = require("../middleware/auth.middleware");
 const postAuth = require("../middleware/auth.middleware");
 const uploadAuth = require("../middleware/auth.middleware");
 const uploadController = require("../controllers/upload.controller");
 
 //Posts
-router.post("/", upload.single("picture"), postController.createPost);
-router.get("/", postController.readPost);
+router.post("/", upload.single("picture"),auth.checkUser, postController.createPost);
+router.get("/", auth.checkUser,postController.readPost);
 router.put("/:id", postAuth.isPostAuth, postController.updatePost);
 router.delete("/:id", postAuth.isPostAuth, postController.deletePost);
 
@@ -17,16 +17,15 @@ router.delete("/:id", postAuth.isPostAuth, postController.deletePost);
 router.post(
     "/upload",
     upload.single("file"),
-    
     uploadController.updatePicture,
   );
 
 //likes/unlikes
-router.patch("/like-post/:id",postController.likePost);
-router.patch("/unlike-post/:id", postController.unlikePost);
+router.patch("/like-post/:id",auth.checkUser,postController.likePost);
+router.patch("/unlike-post/:id",auth.checkUser, postController.unlikePost);
 
 //Commentaires
-router.patch("/comment-post/:id", postController.commentPost);
+router.patch("/comment-post/:id",auth.checkUser, postController.commentPost);
 router.put("/comment-edit/:id/:commentId/:text",postAuth.isPostAuth, postController.updateComment);
 router.delete("/comment-delete/:id/:commentId", postAuth.isPostAuth,postController.deleteComment);
 
